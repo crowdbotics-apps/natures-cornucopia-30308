353e841940784bde186730da5133251a2da96e13
Crowdbotics <team@crowdbotics.com>
2021-09-04 03:17:57 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "natures_cornucopia_30308Identifier",
+  url: "https://natures_cornucopia_30308Identifier.botics.co",
+  api: "https://natures_cornucopia_30308Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
