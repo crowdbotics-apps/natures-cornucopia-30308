847053dad41bda7122bc15710e0e9e2bf89d6882
Crowdbotics <team@crowdbotics.com>
2021-09-04 03:19:52 +0000

    Update models

--- backend/natures_cornucopia_30308/wsgi.py
+++ backend/natures_cornucopia_30308/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'natures_cornucopia_30308.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "natures_cornucopia_30308.settings")
 
 application = get_wsgi_application()
